2366dd465e1ea1ee24aa439d9c91df3af2db63f3
Frédéric Bodin <frederic.bodin@altran.com>
2017-01-12 11:28:20 +0100

    Initial push
    Contains the NFluent skeleton with basic tests

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace Project
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void ProblemSolving1()
+        {
+            var pos = Solution.GetPositionAt(3);
+            Check.That(pos).Equals(-4);
+        }
+
+        [TestMethod]
+        public void ProblemSolving2()
+        {
+            var pos = Solution.GetPositionAt(100000);
+            Check.That(pos).Equals(-5);
+        }
+
+        [TestMethod]
+        public void ProblemSolving3()
+        {
+            var pos = Solution.GetPositionAt(2147483647);
+            Check.That(pos).Equals(1);
+        }
+    }
+}
