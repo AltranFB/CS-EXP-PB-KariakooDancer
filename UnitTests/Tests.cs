7282742ad5b5d7f35927f463e17b78087765ca1e
Frédéric Bodin <frederic.bodin@altran.com>
2017-01-12 12:22:59 +0100

    Working solution

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -14,6 +14,13 @@ namespace Project
         }
 
         [TestMethod]
+        public void ProblemSolving4()
+        {
+            var pos = Solution.GetPositionAt(4);
+            Check.That(pos).Equals(-5);
+        }
+
+        [TestMethod]
         public void ProblemSolving2()
         {
             var pos = Solution.GetPositionAt(100000);
@@ -25,6 +32,27 @@ namespace Project
         {
             var pos = Solution.GetPositionAt(2147483647);
             Check.That(pos).Equals(1);
+        }
+
+        [TestMethod]
+        public void Reliability1()
+        {
+            var pos = Solution.GetPositionAt(0);
+            Check.That(pos).Equals(0);
+        }
+
+        [TestMethod]
+        public void Reliability2()
+        {
+            var pos = Solution.GetPositionAt(1);
+            Check.That(pos).Equals(1);
+        }
+
+        [TestMethod]
+        public void Reliability3()
+        {
+            var pos = Solution.GetPositionAt(2);
+            Check.That(pos).Equals(-1);
         }
     }
 }
